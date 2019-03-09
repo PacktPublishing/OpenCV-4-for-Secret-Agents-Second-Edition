134b8794515c8453be4a14f7452daa0268caa595
Joe Howse <josephhowse@nummist.com>
2019-03-09 00:50:31 -0400

    Requesting Android camera permissions before launching Rollingball scene

---
+++ Chapter006/Rollingball/Unity/Assets/Rollingball/Scripts/Launcher.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#if PLATFORM_ANDROID
+using UnityEngine.Android;
+#endif
+
+
+public class Launcher : MonoBehaviour {
+
+    void Start() {
+
+#if PLATFORM_ANDROID
+        // Ask the user's permission for camera access.
+        Permission.RequestUserPermission(Permission.Camera);
+#endif
+
+        SceneManager.LoadScene("Rollingball");
+    }
+}
