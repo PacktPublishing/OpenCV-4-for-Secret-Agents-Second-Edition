ae6c5e3e4b6a7435ca91958d21b8984b47a6864f
Joe Howse <josephhowse@nummist.com>
2019-03-16 15:09:14 -0300

    Modifying BinasciiUtils to support both Python 2 and 3

--- PythonUtils/BinasciiUtils.py
+++ PythonUtils/BinasciiUtils.py
@@ -2,7 +2,7 @@ import binascii
 
 
 def fourCharsToInt(s):
-    return int(binascii.hexlify(s), 16)
+    return int(binascii.hexlify(bytearray(s, 'ascii')), 16)
 
 def intToFourChars(i):
-    return binascii.unhexlify(format(i, 'x'))
+    return binascii.unhexlify(format(i, 'x')).decode('ascii')
