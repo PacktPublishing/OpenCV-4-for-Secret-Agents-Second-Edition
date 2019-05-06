797ef1a797911c2dc97c1c65409ded1ec674bbf2
Joe Howse <josephhowse@nummist.com>
2019-05-05 23:20:27 -0300

    Updating links to Microsoft Cat Dataset 2008 in download_datasets.sh

--- Chapter003/cascade_training/download_datasets.sh
+++ Chapter003/cascade_training/download_datasets.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-wget https://web.archive.org/web/20150520175645/http://137.189.35.203/WebUI/CatDatabase/Data/CAT_DATASET_01.zip
-wget https://web.archive.org/web/20150520175645/http://137.189.35.203/WebUI/CatDatabase/Data/CAT_DATASET_02.zip
+wget https://archive.org/download/CAT_DATASET/CAT_DATASET_01.zip
+wget https://archive.org/download/CAT_DATASET/CAT_DATASET_02.zip
 wget http://www.vision.caltech.edu/Image_Datasets/faces/faces.tar
 wget -r --no-parent --reject "index.html*" http://vision.cs.utexas.edu/voc/VOC2007_test/
 svn export https://github.com/sonots/tutorial-haartraining/trunk/data/negatives urtho_negatives
